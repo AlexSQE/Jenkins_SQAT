8b38ca8762c212d6732a0c7cf82edab6fb680a26
Elena Kanash <78100493+ElenaKanash@users.noreply.github.com>
2023-07-25 21:26:12 +0300

    RF| AT_20.04.01  change number from AT_20.04.001 (#1036)
    * AT_20.04.01  change number from AT_20.04.001
    
    * add items length assert
    
    * remove only

--- cypress/e2e/tests/dashboard.cy.js
+++ cypress/e2e/tests/dashboard.cy.js
@@ -8,12 +8,13 @@ describe('dashboard', () => {
 
     const homePage = new HomePage()
 
-    it("AT_20.04.001 | <Dashboard> Jenkins Table:: Pipeline's dropdown menu", () => {
+    it("AT_20.04.01 | <Dashboard> Jenkins Table: Pipeline's dropdown menu", () => {
         cy.createPipeline(pipelinePageData.pipelineName);
         homePage
             .hoverAndClickProjectDrpDwn(pipelinePageData.pipelineName)
             .verifyPipeLineDrpDwnMenu()
             .should('deep.equal', pipelinePageData.pipelineDropdownItems)
+            .and('have.length', pipelinePageData.pipelineDropdownItems.length)
     })
 
     it('AT20.01.007.1|DashbordVerify size of project table S', () => {
